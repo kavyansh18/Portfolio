205ff2e24f12566745bd08abbd5a11ad9683a9b6
kavyansh18 <kavyanshkumar18@gmail.com>
2024-07-09 18:41:44 +0530

    fix/toggle

--- client/src/Pages/Homepg.tsx
+++ client/src/Pages/Homepg.tsx
@@ -1,5 +1,6 @@
 import ToggleSwitch from "../Components/ToggleSwitch";
 import Navbaar from "../Components/Navbaar.tsx";
+import '../Components/UI/ToggleSwitch.css'
 
 export function Homepg() {
   return (
@@ -9,7 +10,7 @@ export function Homepg() {
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] "></div>
         <p className="text-4xl sm:text-7xl font-bold relative  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8 ">
-          Hi, Myself Kavyansh
+          Hi, I'm Kavyansh
         </p>
         <ToggleSwitch />
       </div>
