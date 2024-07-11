9bb51eb94a99df421f5651db4b05de89855c4cc5
kavyansh18 <kavyanshkumar18@gmail.com>
2024-07-11 13:44:49 +0530

    feat/pfp

--- client/src/Pages/Homepg.tsx
+++ client/src/Pages/Homepg.tsx
@@ -4,6 +4,7 @@ import '../Components/UI/ToggleSwitch.css'
 "use client";
 import { motion } from "framer-motion";
 import { HeroHighlight, Highlight } from "../Components/UI/Hero-highlight.tsx";
+import pp from "../assets/profilepic.jpg"
 
 export function Homepg() {
   return (
@@ -23,9 +24,10 @@ export function Homepg() {
           duration: 0.5,
           ease: [0.4, 0.0, 0.2, 1],
         }}
-        className="text-4xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto  "
+        className="text-4xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto pb-[110px] "
       >
-        Hi!, I'am Kavyansh <br></br>
+        <div className="flex justify-center items-center mb-2"><img className="w-[170px] rounded-full" src={pp} alt="" /></div>
+        Hi, I'am Kavyansh <br></br>
         <Highlight className="text-black dark:text-white">
           A FullStack Web Developer
         </Highlight>
