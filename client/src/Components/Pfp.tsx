c37187ca7932c039dc5ccbe2d9b177d01d545c00
kavyansh18 <kavyanshkumar18@gmail.com>
2024-07-11 20:42:01 +0530

    chng/pfp-ani

---
+++ client/src/Components/Pfp.tsx
@@ -0,0 +1,22 @@
+import '../Components/UI/Pfp.css'
+const Pfp = () => {
+  return (
+    <div>
+    <div className="conteudo__geral">
+        <div className="cartao cartao0">
+            <div className="elemento__interno-borda">
+                <h2 className='text-[12px] mt-3'>Let's talk about Virat & code</h2>
+                <div className="caixa__icones">
+                    <i className="fa fa-github " aria-hidden="true"></i>
+                    <i className="fa fa-instagram" aria-hidden="true"></i>
+                    <i className="fa fa-linkedin" aria-hidden="true"></i>
+                    <i className="fa fa-twitter" aria-hidden="true"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Pfp
--- client/src/Pages/Homepg.tsx
+++ client/src/Pages/Homepg.tsx
@@ -1,38 +1,40 @@
 import ToggleSwitch from "../Components/ToggleSwitch";
 import Navbaar from "../Components/Navbaar.tsx";
-import '../Components/UI/ToggleSwitch.css'
-"use client";
+import "../Components/UI/ToggleSwitch.css";
+("use client");
 import { motion } from "framer-motion";
 import { HeroHighlight, Highlight } from "../Components/UI/Hero-highlight.tsx";
-import pp from "../assets/profilepic.jpg"
+import Pfp from '../Components/Pfp.tsx'
 
 export function Homepg() {
   return (
     <div className="relative h-screen">
       <Navbaar />
       <HeroHighlight>
-      <motion.h1
-        initial={{
-          opacity: 0,
-          y: 20,
-        }}
-        animate={{
-          opacity: 1,
-          y: [20, -5, 0],
-        }}
-        transition={{
-          duration: 0.5,
-          ease: [0.4, 0.0, 0.2, 1],
-        }}
-        className="text-4xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto pb-[110px] "
-      >
-        <div className="flex justify-center items-center mb-2"><img className="w-[170px] rounded-full" src={pp} alt="" /></div>
-        Hi, I'am Kavyansh <br></br>
-        <Highlight className="text-black dark:text-white">
-          A FullStack Web Developer
-        </Highlight>
-      </motion.h1>
-    </HeroHighlight>
+        <motion.h1
+          initial={{
+            opacity: 0,
+            y: 20,
+          }}
+          animate={{
+            opacity: 1,
+            y: [20, -5, 0],
+          }}
+          transition={{
+            duration: 0.5,
+            ease: [0.4, 0.0, 0.2, 1],
+          }}
+          className="text-4xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto pb-[110px] "
+        >
+          <div className="flex justify-center items-center">
+            <Pfp />
+          </div>
+          Hi, I'am Kavyansh <br></br>
+          <Highlight className="text-black dark:text-white">
+            A FullStack Web Developer
+          </Highlight>
+        </motion.h1>
+      </HeroHighlight>
       <ToggleSwitch />
     </div>
   );
