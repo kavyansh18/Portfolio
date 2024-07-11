fc0371aed3b3f42b659925aee95ef1c44f6d8683
kavyansh18 <kavyanshkumar18@gmail.com>
2024-07-12 00:14:25 +0530

    feat/about

--- client/src/Pages/Homepg.tsx
+++ client/src/Pages/Homepg.tsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { HeroHighlight, Highlight } from "../Components/UI/Hero-highlight.tsx";
 import Pfp from '../Components/Pfp.tsx';
 import down from "../assets/downarrow.svg"
+import { Sparkles } from "../Components/UI/Sparkles.tsx";
 
 export function Homepg() {
   return (
@@ -39,8 +40,31 @@ export function Homepg() {
           <div className="w-full flex justify-center items-center mt-12">
           <img className="w-[3rem]" src={down} alt="" /></div>
         </motion.h1>
-      </HeroHighlight>
 
+      <div className="flex">
+        <div>
+      <h1 className="md:text-7xl text-lg lg:text-4xl font-bold text-white relative z-20 ml-20 mt-8">
+        About me
+      </h1>
+      <div className="w-[8.5rem] h-[36px] relative ml-24">
+        <div className="absolute inset-x-5 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+        <div className="absolute inset-x-3 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[3px] w-3/4" />
+        <Sparkles
+          background="transparent"
+          minSize={0.4}
+          maxSize={1}
+          particleDensity={1200}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+          />
+
+        <div className="absolute inset-0 w-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+        </div>
+      </div>
+
+      <div></div>
+      </div>
+      </HeroHighlight>
       <ToggleSwitch />
     </div>
   );
