2261edee568bf03f4823a64264edb507dc64a5cf
kavyansh18 <kavyanshkumar18@gmail.com>
2024-07-17 20:17:58 +0530

    feat/timeline-temp

--- client/src/Pages/Experience.tsx
+++ client/src/Pages/Experience.tsx
@@ -1,4 +1,16 @@
-import { BackgroundBeams } from '../Components/UI/Background-beams';
+import { BackgroundBeams } from "../Components/UI/Background-beams";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+import LaptopMacIcon from "@mui/icons-material/LaptopMac";
+import HotelIcon from "@mui/icons-material/Hotel";
+import Typography from "@mui/material/Typography";
+import RepeatIcon from '@mui/icons-material/Repeat';
 
 const Experience = () => {
   return (
@@ -6,11 +18,108 @@ const Experience = () => {
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="relative z-10 text-lg md:text-3xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold mt-14">
           Experience
-    </h1>
+        </h1>
+      </div>
+      <div>
+        <Timeline position="alternate">
+          <TimelineItem>
+            <TimelineOppositeContent
+              sx={{ m: "auto 0" }}
+              align="right"
+              variant="body2"
+              color="blue"
+            >
+              Busy&Organised(B&O)
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineConnector />
+              <TimelineDot>
+                <FastfoodIcon />
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ py: "12px", px: 2 }}>
+              <Typography variant="h6" component="span" color="blue">
+                Web Developer Intern
+              </Typography>
+              <Typography color="blue">1st February'24 - 31st February'24</Typography>
+              <Typography color="blue">Because you need strength</Typography>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent
+              sx={{ m: "auto 0" }}
+              variant="body2"
+              color="blue"
+            >
+              NullClass
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineConnector />
+              <TimelineDot color="primary">
+                <LaptopMacIcon />
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ py: "12px", px: 2 }}>
+              <Typography variant="h6" component="span" color="blue">
+              Web Developer Intern
+              </Typography>
+              <Typography color="blue">10th June'24 - 9th June'24</Typography>
+              <Typography color="blue">Because it&apos;s awesome!</Typography>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+          <TimelineOppositeContent
+              sx={{ m: "auto 0" }}
+              variant="body2"
+              color="blue"
+            >
+              The Product House(TPHxSRMIST)
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineConnector />
+              <TimelineDot color="primary" variant="outlined">
+                <HotelIcon />
+              </TimelineDot>
+              <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+            </TimelineSeparator>
+            <TimelineContent sx={{ py: "12px", px: 2 }}>
+              <Typography variant="h6" component="span" color="blue">
+                Technical Team Member
+              </Typography>
+              <Typography color="blue">December'24 - present</Typography>
+              <Typography color="blue">Because you need rest</Typography>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+          <TimelineOppositeContent
+              sx={{ m: "auto 0" }}
+              variant="body2"
+              color="blue"
+            >
+              HackTheBox
+            </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+          <TimelineDot color="secondary">
+            <RepeatIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span" color="blue">
+          Technical Team Member
+          </Typography>
+          <Typography color="blue">March'24 - present</Typography>
+          <Typography color="blue">Because this is the life you love!</Typography>
+        </TimelineContent>
+      </TimelineItem>
+        </Timeline>
       </div>
       <BackgroundBeams />
     </div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
