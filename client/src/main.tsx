2f3c3551332d4666b18bb5114958eda4f3f40aca
kavyansh18 <kavyanshkumar18@gmail.com>
2024-08-04 12:28:40 +0530

    fix:reload-not-found

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
 } from "react-router-dom";
